f360a44e119dfda4d0add873c891f77c2ed217da
Matheus Diniz <dinizmaths@gmail.com>
2022-03-22 13:25:25 -0300

    create a base html

--- receitas/urls.py
+++ receitas/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .           import views
 
 urlpatterns = [
-    path('', view=views.index, name='index')
+    path('', view=views.index, name='index'),
+    path('/rececita', view=views.receita, name='receita')
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+import receitas
+
 def index(request):
   return render(request, 'index.html')
+
+def receita(request):
+  return render(request, 'receita.html')
