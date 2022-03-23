d2aebd94f93eab2d9bb1f7b79fe8f7c769907ad3
Matheus Diniz <dinizmaths@gmail.com>
2022-03-22 23:24:28 -0300

    database conigure

--- receitas/views.py
+++ receitas/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 import receitas
 
 def index(request):
-  return render(request, 'index.html')
+  receitas = {
+    1: 'Lasanha',
+    2: 'Sopa de legumes',
+    3: 'Sorvete'
+  }
+
+  dados = {
+    'nomes_das_receitas': receitas
+  }
+
+  return render(request=request, template_name='index.html', context=dados)
 
 def receita(request):
-  return render(request, 'receita.html')
+  return render(request, template_name='receita.html')
