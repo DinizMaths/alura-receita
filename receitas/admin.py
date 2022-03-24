ce7c5d9e7c75ea98f940b04ca4249006a87cfcd0
Matheus Diniz <dinizmaths@gmail.com>
2022-03-24 20:06:15 -0300

    admin upgrade

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,13 +1,12 @@
-from dataclasses import field
-from re import search
 from django.contrib import admin
 from .models        import Receita
 
 # Register your models here.
 class ListandoReceitas(admin.ModelAdmin):
-  list_display = ('id', 'nome_receita')
-  list_display_link = ('id', 'nome_receita')
+  list_display = ('id', 'nome_receita', 'publicada')
+  list_display_links = ('id', 'nome_receita')
 
+  list_editable = ('publicada',)
   search_fields = ('nome_receita',)
   list_filter = ('categoria',)
   list_per_page = 2
--- receitas/models.py
+++ receitas/models.py
@@ -1,12 +1,17 @@
-from django.db import models
+from curses import doupdate
+import django
+from django.db      import models
+from pessoas.models import Pessoa
 import django
 
 # Create your models here.
 class Receita(models.Model):
+  pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
   nome_receita = models.CharField(max_length=200)
   ingredientes = models.TextField()
   modo_preparo = models.TextField()
   tempo_preparo = models.IntegerField()
   rendimento = models.CharField(max_length=100)
   categoria = models.CharField(max_length=100)
-  data_receita = models.DateField(default=django.utils.timezone.now, blank=True)
+  data_receita = models.DateTimeField(default=django.utils.timezone.now, blank=True)
+  publicada = models.BooleanField(default=False)
